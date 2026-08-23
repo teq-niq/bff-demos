6fd1da51abc9f5522b13ee63c02f9ca4e7a39289
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2026-08-24 01:03:14 +0530

    trying to improve angularshell

--- oidc.bff/angularshell.sh
+++ oidc.bff/angularshell.sh
@@ -6,10 +6,10 @@ PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # Force angular-front-end as working directory
 cd "$PROJECT_DIR/angular-front-end" || exit
 
-# Isolated PATH (does NOT include existing PATH)
-export PATH="$PROJECT_DIR/../node:$PROJECT_DIR/angular-front-end/node_modules/.bin:$VS_CODE_HOME/bin"
+# Isolated toolchain plus the minimal Linux command paths required by Bash.
+export PATH="$PROJECT_DIR/../node:$PROJECT_DIR/angular-front-end/node_modules/.bin${VS_CODE_HOME:+:$VS_CODE_HOME/bin}:/usr/bin:/bin"
 
-echo "Using isolated Node environment:"
+echo "Using Maven-managed Node environment:"
 node -v
 npm -v
 ng version
